794e7904962be3238fd56d4c50d20181bc4ebca5
Scott Russell <scottedwardrussell@gmail.com>
2019-10-14 11:23:54 -0400

    Added env.sh and mac setup files

---
+++ env.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+export GOPATH="/Users/scott.russell/dev/go"
+export PYTHONPATH="/usr/local/opt/python"
+export PATH="$GOPATH/bin:$PYTHONPATH/libexec/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export EDITOR=vim
+
+# Create a folder and move into it in one command
+function mkcd() { mkdir -p "$@" && cd "$_"; }
+
+alias tsw="tmux split-window"
